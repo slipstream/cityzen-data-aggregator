9ecd41c974f34b82a4d407517a36eadc8f2e494d
schaubl <schaubl@users.noreply.github.com>
2017-03-24 09:28:46 +0100

    Create conf.py

---
+++ xemtec/conf.py
@@ -0,0 +1,20 @@
+
+# Graphite configuration
+
+graphite_address = '127.0.0.1'
+graphite_port = 2003
+
+# Xemtec configuration
+
+name = 'xemtec'
+endpoint = 'http://mydata.xemtec.ch'
+username = ''
+password = ''
+verify_cert = True
+
+readers_name = {}
+readers = None # Autodetect
+
+country = 'CH'
+city = 'mycity'
+
